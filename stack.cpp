5230f49f28c1de8d21618f7f2954f253a3f207d3
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-05 15:12:33 +0700

    Pilihan Keempat

--- stack.cpp
+++ stack.cpp
@@ -90,6 +90,9 @@ int main(){
             case 3:
                 stack.peek();
                 break;
+            case 4:
+                cout << "Exiting Program." << endl;
+                break;
         }
     }
 }
